efe04031208bb1f4f23d864a814c651d8ba3c3ee
merrylandquynhon <99806233+merrylandquynhon@users.noreply.github.com>
2022-07-10 15:21:39 +0700

    Update redirectcopymiamivungtau.js

--- redirectcopymiamivungtau.js
+++ redirectcopymiamivungtau.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://miamivungtau.com'>https://miamivungtau.com"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://fiveseasonhomesvungtau.com'>https://fiveseasonhomesvungtau.com"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
